0f033cd7b6ef0edf6f02fe4ffbf00e5c9b9aedfc
Lucas Gomes <lucasgomes_tl@hotmail.com>
2019-10-12 06:41:09 -0400

    Create development environment

---
+++ README.md
@@ -0,0 +1,5 @@
+# challange-single-price-grid
+This a challange from beta.frontendmentor.io
+
+You can find the challange on:<br>
+https://beta.frontendmentor.io/challenges/single-price-grid-component-5ce41129d0ff452fec5abbbc
---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+
+function style(){ 
+    return new Promise(function(resolve, reject){
+        gulp.src('./sass/**/*.sass')
+        .pipe(sass({ outputStyle : 'compressed'}))
+        .on('error', sass.logError)
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.stream())
+        resolve();
+    });
+}
+
+function watch(){
+    browserSync.init({
+        server:{
+            baseDir:'./'
+        }
+    });
+    gulp.watch('sass/**/*.sass', style);
+    gulp.watch('./**/*.html').on('change', browserSync.reload);
+}
+
+gulp.task('default', gulp.series(style, watch));
